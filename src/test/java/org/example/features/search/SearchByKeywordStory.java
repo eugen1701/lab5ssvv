b27d2dcb2856e2215587bd52a89033576bd9a48e
eugen1701 <eugen.flocea@yahoo.com>
2022-05-09 22:03:35 +0300

    Initial commit

---
+++ src/test/java/org/example/features/search/SearchByKeywordStory.java
@@ -0,0 +1,56 @@
+package org.example.features.search;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Issue;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Pending;
+import net.thucydides.core.annotations.Steps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import org.example.steps.serenity.EndUserSteps;
+
+@RunWith(SerenityRunner.class)
+public class SearchByKeywordStory {
+
+    @Managed(uniqueSession = true)
+    public WebDriver webdriver;
+
+    @Steps
+    public EndUserSteps anna;
+
+    @Issue("#WIKI-1")
+    @Test
+    public void searching_by_keyword_apple_should_display_the_corresponding_article() {
+        anna.is_the_home_page();
+        anna.looks_for("apple");
+        anna.should_see_definition("A common, round fruit produced by the tree Malus domestica, cultivated in temperate climates.");
+
+    }
+
+    @Test
+    public void searching_by_keyword_banana_should_display_the_corresponding_article() {
+        anna.is_the_home_page();
+        anna.looks_for("pear");
+        anna.should_see_definition("An edible fruit produced by the pear tree, similar to an apple but elongated towards the stem.");
+    }
+
+    @Pending @Test
+    public void searching_by_ambiguious_keyword_should_display_the_disambiguation_page() {
+    }
+
+    @Test
+    public void searching_by_keyword_phone_should_display_the_corresponding_article() {
+        anna.is_the_home_page();
+        anna.looks_for("phone");
+        anna.should_see_definition("A device for transmitting conversations and other sounds in real time across distances, now often a small portable unit also capable of running software etc.");
+    }
+    @Test
+    public void searching_by_keyword_box_should_display_the_corresponding_article() {
+        anna.is_the_home_page();
+        anna.looks_for("box");
+        anna.should_see_definition("A cuboid space; a cuboid container, often with a hinged lid. ");
+    }
+} 
